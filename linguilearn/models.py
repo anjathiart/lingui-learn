310945eae7942b6e7dd294cf3b1ced0befa44ef6
Anja Thiart <anjathiart@gmail.com>
2020-10-13 20:53:46 +0200

    Setup templates and initial word and user models

--- linguilearn/models.py
+++ linguilearn/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Word(models.Model):
+	word = models.CharField(max_length=255)
+	definition = models.CharField(max_length=1028)
+	example = models.CharField(max_length=1028)
+	phrase = models.CharField(max_length=1028)
+	word_of_the_day = models.BooleanField(default=False)
+	word_of_the_day_date = models.DateTimeField(auto_now_add=False)
+	def serialize(self):
+		return {
+			"id": self.id,
+			"word": self.sender.email,
+			"definition": [user.email for user in self.recipients.all()],
+			"example": self.subject,
+			"phrase": self.body,
+		}
+
+class User(AbstractUser):
+	followers = models.ManyToManyField("User", related_name="users_following")
+	mastered_words = models.ManyToManyField("Word", related_name="users_mastered")
+	learning_words = models.ManyToManyField("Word", related_name="users_learning")
+	favourite_words = models.ManyToManyField("Word", related_name="users_favourite")
+	def serialize(self):
+		return {
+			"id": self.id,
+			"name": self.username.capitalize(),
+			"followers": [user.id for user in self.followers.all()],
+			"mastered_words": [word.id for word in self.mastered_words.all()],
+			"learning_words": [word.id for word in self.learning_words.all()],
+			"favourite_words": [word.id for word in self.favourite_words.all()],
+		}
+
+
