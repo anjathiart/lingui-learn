ea876ae3404fc5b188e2a9167addfba2bc32fcb1
Anja Thiart <anjathiart@gmail.com>
2020-10-27 21:46:52 +0200

    Minor updates and refactoring / defactoring of views

--- linguilearn/views/words.py
+++ linguilearn/views/words.py
@@ -9,55 +9,37 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods
 
+from linguilearn.exceptions import AlreadyExistsError, DoesNotExistForUser
+
 from ..models import User, Entry
 
-# Oxford Api details
-oxford = {
-	"headers": {"app_id": "4a7b8083", "app_key": "09fd090f4447176a113d44ca173d445b"},
-	"language_code": "en-gb"
-}
-oxford_api_headers = {"app_id": "4a7b8083", "app_key": "09fd090f4447176a113d44ca173d445b"}
+# # Oxford Api details
+# oxford = {
+# 	"headers": {"app_id": "4a7b8083", "app_key": "09fd090f4447176a113d44ca173d445b"},
+# 	"language_code": "en-gb"
+# }
 
-def oxfordEntryToLinguiEntry(entry):
-	return {
-		"word": entry["word"],
-		"definition": entry["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
-	}
-
-def oxford_get_entry(word):
-	endpoint = "entries"
-	url = "https://od-api.oxforddictionaries.com/api/v2/" + endpoint + "/" + oxford["language_code"]+ "/" + word.lower()
-	# url = url + "?fields=definitions"
-	r = requests.get(url, headers = oxford_api_headers)
-	if r.status_code == 200:
-		entry = r.json()
-		return {
-			"word": entry["word"],
-			"definition": entry["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0],
-			"status_code": r.status_code
-		}
-	else:
-		return {
-			"error": "something went wrong",
-			"status_code": r.status_code
-		}
 
 def oxford_search(word):
-	# result = { "word": word }
-
-	endpoint = "lemmas"
-	url = "https://od-api.oxforddictionaries.com/api/v2/" + endpoint + "/" + "en" + "/" + word.lower()
-	r = requests.get(url, headers = oxford_api_headers)
+	# Oxford API details
+	oxford = {
+		"headers": {"app_id": "4a7b8083", "app_key": "09fd090f4447176a113d44ca173d445b"},
+		"language_code": "en-gb"
+	}
+	url = "https://od-api.oxforddictionaries.com/api/v2/lemmas/en/" + word.lower()
+	r = requests.get(url, headers = oxford["headers"])
 	if r.status_code == 200:
-		lex_result = r.json() 
+		lex_result = r.json()
 		head_word = lex_result["results"][0]["lexicalEntries"][0]["inflectionOf"][0]["text"]
-		entry = oxford_get_entry(head_word)
-		entry["head_word"] = head_word
-		return entry
-	# elif r.status_code == 500:
-	# 	return { "error": 500 }
-	else:
-		return None
+		url = "https://od-api.oxforddictionaries.com/api/v2/entries/" + oxford["language_code"]+ "/" + head_word.lower()
+		r = requests.get(url, headers = oxford["headers"])
+		if r.status_code == 200:
+			entry = r.json()
+			return {
+				"word": entry["word"],
+				"definition": entry["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0],
+			}
+	return None
 
 
 
@@ -73,11 +55,11 @@ def search_entry(request):
 	except Entry.DoesNotExist:
 		entry = oxford_search(word_searched.lower())
 		if entry:
-			# save entry to db
-			entry = Entry(word=entry["head_word"], definition=entry["definition"])
+			entry = Entry(word=entry["word"], definition=entry["definition"])
 			entry.save()
 			ctx["result"] = entry.serialize()
 			return JsonResponse(ctx, status=200)
+		ctx["error"] = "No match was found for the search query"
 		return JsonResponse(ctx, status=404)
 
 
@@ -89,7 +71,7 @@ def add_entry(request, entry_id):
 	try:
 		entry = Entry.objects.get(id=entry_id)
 	except Entry.DoesNotExist:
-		ctx["errors"] = ["Word does not exist"]
+		ctx["error"] = "Word does not exist"
 		return JsonResponse(ctx, status=404)
 
 	request.user.entries.add(entry)
@@ -113,7 +95,7 @@ def remove_entry(request, entry_id):
 	try:
 		request.user.remove_entry(entry_id)
 	except Entry.DoesNotExist:
-		ctx["errors"] = ["Word does not exist"]
+		ctx["error"] = "Word does not exist"
 		return JsonResponse(ctx, status=404)
 
 	# request.user.learning_words.add(entry)
@@ -127,8 +109,8 @@ def master_entry(request, entry_id):
 
 	try:
 		request.user.master_entry(entry_id)
-	except Entry.DoesNotExist:
-		ctx["error"] = "No entry matching request entry id"
+	except DoesNotExistForUser as e:
+		ctx["error"] = "%s" % e
 		return JsonResponse(ctx, status=404)
 
 	# request.user.learning_words.add(entry)
