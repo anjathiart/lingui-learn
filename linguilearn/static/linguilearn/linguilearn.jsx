6eb6337871f205fa2cb7981f68fdfffcbd803867
Anja Thiart <anjathiart@gmail.com>
2020-10-31 21:08:11 +0200

    WIP searching and viewing users to send friend requests

--- linguilearn/static/linguilearn/linguilearn.jsx
+++ linguilearn/static/linguilearn/linguilearn.jsx
@@ -8,23 +8,25 @@ const secureFetch = (url, method, data) => {
 			body: JSON.stringify(data),
 			headers: { "X-CSRFToken": csrftoken },
 			credentials: 'same-origin',
-		}).then(response => {
+		}).then(async (response) => {
 			if (response.ok) {
 				// All 200 errors will have response === ok
 				ReactDOM.unmountComponentAtNode(document.getElementById('error__component'));
-				resolve(response.json());
+				resolve( await response.json());
 				return;
 			} else if (response.status === 401) {
 				// All 401's redirect user to login
 				window.location.replace('/login');
 				return;
 			} else {
+				console.log(response)
 				// Deal with other errors from the server / api
-				reject(response.json());
+				reject(await response.json());
 				return;
 			}
 		}).catch(error => {
 			// deal with network errors
+
 			reject({ error });
 		});
 	});
@@ -46,7 +48,8 @@ function myInitCode() {
 	document.querySelector('#searchForUserButton').addEventListener('click', async (e) => {
 		const usernameOrEmail = document.querySelector('#searchForUserInput').value;
 		let result = await userSearch(usernameOrEmail)
-		
+		console.log(result)
+		renderUserList(result.result)
 
 	})
 }
@@ -59,6 +62,97 @@ async function userSearch(search) {
 	})
 	return res
 }
+
+
+
+class Error extends React.Component {
+	render() {
+		return (
+			<div className="modal">
+				<div className="modal__content">
+					<p>{ msg }</p>
+					<button onClick={ this.close } className="btn btn-primary btn-lg">OK</button>
+				</div>
+			</div>
+		);
+	}
+	close = () => {
+		ReactDOM.unmountComponentAtNode(document.getElementById('error__component'));
+	}
+}
+
+function addFriend(user_id) {
+	alert(user_id)
+}
+
+function renderUserList(users) {
+	console.log(users)
+
+
+	class User extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				friendStatus: '',
+			}
+		}
+
+
+		render() {
+			return (
+				<div className="userSearchResultCard">
+					<p>Username: { this.props.user.name }</p>
+					<button onClick={ this.actionAddFriend.bind(this, this.props.user.name) }>Add friend</button>
+					<button onClick={ this.actionRemoveFriend.bind(this, this.props.user.name) }>Remove friend</button>
+				</div>
+			)
+		};
+
+		actionAddFriend = async (username) => {
+			console.log(username)
+			let result = await secureFetch(`api/friendship/${username}/add`, 'POST').catch( e => {
+				console.log(e);
+			})
+			console.log(result)
+
+		};
+
+		actionRemoveFriend = async (username) => {
+			console.log(username)
+			let result = await secureFetch(`api/friendship/${3}/cancel`, 'POST').catch( e => {
+				console.log(e);
+			})
+			console.log(result)
+
+		};
+
+	}
+
+	let list = [];
+	users.forEach(user => {
+		list.push(<User user={ user } key={ user.id }/>)
+	})
+
+
+
+
+
+	class UserList extends React.Component {
+		render() {
+			return (
+				<div>{ list }</div>
+			)
+		}
+	}
+
+	ReactDOM.render(<UserList />, document.querySelector("#userSearchList__component"));
+
+
+}
+
+
+
+
 
 
 // 	document.querySelector('#submitRequest').addEventListener('click', () => {
