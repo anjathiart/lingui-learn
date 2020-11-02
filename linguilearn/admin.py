807512993a0a41c609a75933aa31483264ed41fb
Anja Thiart <anjathiart@gmail.com>
2020-11-02 21:27:23 +0200

    Setup admin models with my models

--- linguilearn/admin.py
+++ linguilearn/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Word, Entry
+
+admin.site.register(User)
+admin.site.register(Word)
+admin.site.register(Entry)
